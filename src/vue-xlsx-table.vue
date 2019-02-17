7995a1d0cfcb08b564d2b0c7f280bf01c8942750
Jesse Knowles <jesse@kinoli.com>
2019-02-16 22:57:19 -0700

    Cleanup styles

--- src/vue-xlsx-table.vue
+++ src/vue-xlsx-table.vue
@@ -158,12 +158,10 @@ export default {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  *{
+  .c-hide{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-  }
-  .c-hide{
     display: none;
   }
 }
